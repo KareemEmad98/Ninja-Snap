1038a285be38aca96192c0a532f9fde11fcd24c0
KareemEmad98 <33865783+KareemEmad98@users.noreply.github.com>
2024-05-07 03:10:24 +0300

    Added Auth utils class to handle Snapchat auth

---
+++ app/utils/auth.py
@@ -0,0 +1,28 @@
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import LegacyApplicationClient
+
+from app import Config
+
+
+class OAuthHandler:
+    def __init__(self):
+        client = LegacyApplicationClient(client_id=Config.CLIENT_ID)
+        self.oauth_session = OAuth2Session(client=client)
+        self.refresh_access_token()
+
+    def refresh_access_token(self):
+        extra = {
+            'client_id': Config.CLIENT_ID,
+            'client_secret': Config.CLIENT_SECRET,
+            'refresh_token': Config.REFRESH_TOKEN
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        print(self.oauth_session.refresh_token(Config.TOKEN_URL, **extra, headers=headers))
+
+    def get_session_with_headers(self):
+        access_token = self.oauth_session.token['access_token']
+        headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/json'}
+        self.oauth_session.headers.update(headers)
+        return self.oauth_session
